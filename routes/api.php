a013fd0dbada8963c73d7726f90c130485646866
Guto Leal <guto.leal@outlook.com.br>
2025-07-22 18:32:35 -0300

    commit inicial

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get("/", function() {
+    return Response::json(['message' => 'Welcome to the API']);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
